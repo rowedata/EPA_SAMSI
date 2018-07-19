d4a3e33613f19bb22462451377d1e6cf0c610c11
Kartik Lovekar <kslovekar@gmail.com>
2018-07-19 18:06:51 -0400

    Changed estimate function definition

--- analysis.R
+++ analysis.R
@@ -10,14 +10,14 @@ NW.NR.AQS.sd <- matrix(c(NW.NR$s1, NW.NR$s2), ncol = 2)
 NW.NR.AQS.d <- matrix(c(NW.NR$d.to.R_1.edge, NW.NR$d.to.R_2.edge), ncol = 2)
 
 phi.mle <- (coef(mle(likelihoodFunction, start = list(phi = 1), fixed = list(
-  Y = NW.NR.AQS, d = NW.NR.AQS.d, mu = NW.NR.AQS.mu, sd = NW.NR.AQS.sd))))[1]
+  Y = NW.NR.AQS, dist = NW.NR.AQS.d, mu = NW.NR.AQS.mu, sd = NW.NR.AQS.sd))))[1]
 
 NW.NR.DS <- read.csv("D:/Ohio State/SAMSI IMSM/EPA_SAMSI/data/combinedOverlap.csv")
 
 DS.d <- matrix(c(118 + NW.NR.DS$Longitude, -1*NW.NR.DS$Longitude - 109), ncol = 2)
 DS.y <- matrix(c(NW.NR.DS$Prediction, NW.NR.DS$Prediction.1), ncol = 2)
 
-spliced.NW.NR <- smoothEstimate(DS.d, DS.y)
+spliced.NW.NR <- smoothEstimate(DS.d, DS.y, phi.mle)
 
 x <- seq(-50, 100, length = 1000)
 NW.pdf <- dnorm(x, mean = NW.NR.DS$Prediction[2], sd = NW.NR.DS$SEpred[2])
