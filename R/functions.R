7599c9c10ddaf72bcdd3f60662c16b5d2acce7f0
Unknown <leejeo25@msu.edu>
2018-07-21 10:00:08 -0400

    add new likelihood

--- R/functions.R
+++ R/functions.R
@@ -42,6 +42,35 @@ likelihoodFunRV <-function(phi, Y, dist, mu, sd){
   return(-(log(likelihood)))
 }
 
+likelihoodFunRV2 <-function(a1,a2,dist.cen, Y, dist, mu, sd){  
+  #Returns the likelihood function for the weighted average of densities
+  #
+  #Args:
+  #  
+  #  a1: Parameter to be estimated, intercept in phi
+  #  a2: parameter to be estimated, slope in phi
+  #  (phi: a1+a2xdist.cen)
+  #  Y: AQS readings corresponding to DS estimates. Nx2 matrix.
+  #  dist: Distance, measured in degrees of longitude, from each AQS point to
+  #     boundaries of the overlap region. Nx2 matrix.
+  #  mu: Downscaler estimates corresponding to AQS readings, Nx2 matrix.
+  #  sd: Downscaler standard errors, Nx2 matrix.
+  #  dist.cen:distnace measured from boundary center to the point. nx1 vector
+  #Returns:
+  #  The log likelihood for phi
+    phi<-a1+a2*dist.cen
+   w <- exp(-phi*dist)
+  new.mu <- apply(w*mu, 1, sum)
+  new.s <- apply((w^2)*(sd^2), 1, sum)
+  new.s <- (new.s)^{1/2}
+  likelihood <- prod(dnorm(Y, new.mu, new.s))
+  return(-(log(likelihood)))
+}
+
+
+
+
+
 smoothEstimate <- function(dist, mu, phi){
   #Returns combined estimates for each grid value in the intersection
   #
