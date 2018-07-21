cf668d407cd833bed35cd59072a807ab3eec576b
Kartik Lovekar <kslovekar@gmail.com>
2018-07-21 09:42:01 -0400

    Added mixture model to functions file.

--- R/functions.R
+++ R/functions.R
@@ -3,7 +3,7 @@ setwd("D:/Ohio State/SAMSI IMSM/EPA_SAMSI")
 
 library(stats4)
 
-likelihoodFunction <- function(phi, Y, dist, mu, sd){
+likelihoodFunDensity <- function(phi, Y, dist, mu, sd){
   #Returns the likelihood function for the weighted average of densities
   #
   #Args:
@@ -21,6 +21,27 @@ likelihoodFunction <- function(phi, Y, dist, mu, sd){
   return(-(log(likelihood)))
 }
 
+likelihoodFunRV <-function(phi, Y, dist, mu, sd){  
+  #Returns the likelihood function for the weighted average of densities
+  #
+  #Args:
+  #  phi: Parameter to be estimated
+  #  Y: AQS readings corresponding to DS estimates. Nx2 matrix.
+  #  dist: Distance, measured in degrees of longitude, from each AQS point to
+  #     boundaries of the overlap region. Nx2 matrix.
+  #  mu: Downscaler estimates corresponding to AQS readings, Nx2 matrix.
+  #  sd: Downscaler standard errors, Nx2 matrix.
+  #
+  #Returns:
+  #  The log likelihood for phi
+  w <- exp(-phi*dist)
+  new.mu <- apply(w*mu, 1, sum)
+  new.s <- apply((w^2)*(sd^2), 1, sum)
+  new.s <- (new.s)^{1/2}
+  likelihood <- prod(dnorm(Y, new.mu, new.s))
+  return(-(log(likelihood)))
+}
+
 smoothEstimate <- function(dist, mu, phi){
   #Returns combined estimates for each grid value in the intersection
   #
@@ -31,7 +52,7 @@ smoothEstimate <- function(dist, mu, phi){
   #  phi: MLE of parameter phi
   #
   #Returns:
-  # Combined estimates for each grid point
+  # Combined estimates for each grid point, Nx1 vector
   w <- exp(-1*phi*dist)
   estimate <- (1/apply(w, 1, sum))*apply(w*mu, 1, sum)
   return(estimate)
--- analysis.R
+++ analysis.R
@@ -1,6 +1,7 @@
 
 source("R/functions.R")
 
+##Reading AQS Grid Data
 AQS.grid <- read.csv("D:/Ohio State/SAMSI IMSM/EPA_SAMSI/data/AQS_DATA.csv")
 NW.NR <- read.csv("D:/Ohio State/SAMSI IMSM/EPA_SAMSI/data/combinedOfNW_NR.csv")
 
@@ -9,16 +10,24 @@ NW.NR.AQS.mu <- matrix(c(NW.NR$m1, NW.NR$m2), ncol = 2)
 NW.NR.AQS.sd <- matrix(c(NW.NR$s1, NW.NR$s2), ncol = 2)
 NW.NR.AQS.d <- matrix(c(NW.NR$d.to.R_1.edge, NW.NR$d.to.R_2.edge), ncol = 2)
 
+##Compute MLE
 phi.mle <- (coef(mle(likelihoodFunction, start = list(phi = 1), fixed = list(
   Y = NW.NR.AQS, dist = NW.NR.AQS.d, mu = NW.NR.AQS.mu, sd = NW.NR.AQS.sd))))[1]
 
+##Read all DS estimates for region
 NW.NR.DS <- read.csv("D:/Ohio State/SAMSI IMSM/EPA_SAMSI/data/combinedOverlap.csv")
 
+##Calculate distance from boundaries
 DS.d <- matrix(c(118 + NW.NR.DS$Longitude, -1*NW.NR.DS$Longitude - 109), ncol = 2)
+
+##Read DS estimates and standard errors
 DS.y <- matrix(c(NW.NR.DS$Prediction, NW.NR.DS$Prediction.1), ncol = 2)
+DS.se <- matrix(c(NW.NR.DS$SEpred, NW.NR.DS$SEpred.1), ncol = 2)
 
-spliced.NW.NR <- smoothEstimate(DS.d, DS.y, phi.mle)
+##Get estimates based on MLE
+spliced.NW.NR.D <- smoothEstimate(DS.d, DS.y, phi.mle)
 
+##Plot combined density function
 x <- seq(-50, 100, length = 1000)
 NW.pdf <- dnorm(x, mean = NW.NR.DS$Prediction[2], sd = NW.NR.DS$SEpred[2])
 NR.pdf <- dnorm(x, mean = NW.NR.DS$Prediction.1[2], sd = NW.NR.DS$SEpred.1[2])
@@ -30,3 +39,8 @@ plot(x, NW.pdf, type = "l", ylab = "NW")
 plot(x, NR.pdf, type = "l", ylab = "NR")
 plot(x, spliced.pdf, type = "l", ylab = "Spliced")
 
+##Get smoothed estimates from mixture model
+phi.mle.2 <- (coef(mle(combineRVLL, start = list(phi = .4), fixed = list(
+  Y = NW.NR.AQS2, dist = NW.NR.AQS.d, mu = NW.NR.AQS.mu, sd = NW.NR.AQS.sd))))[1]
+
+spliced.NW.NR.RV <- smoothEstimate(DS.d, DS.y, phi.mle2)
